c62b376fdccd265ab1adf8faf94f40c48690fd81
lyndonl3091 <lyndonl3091@gmail.com>
2017-01-19 19:03:28 -0500

    basic problems

--- basics/index.py
+++ basics/index.py
@@ -8,3 +8,20 @@ def sum_two(num1, num2):
     return num1 + num2
 
 print(sum_two(1, 3))
+print(sum_two(100, 325))
+
+# square
+
+def square_num(num):
+    return num * num
+
+print(square_num(5))
+print(square_num(10))
+
+# check if even
+
+def check_even(num):
+    return num % 2 == 0
+
+print check_even(2)
+print check_even(5)
