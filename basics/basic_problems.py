27e26fa2f3a2b4cad66152bec9bf6cf75b97dbc9
lyndonl3091 <lyndonl3091@gmail.com>
2017-01-19 19:24:11 -0500

    max

--- basics/basic_problems.py
+++ basics/basic_problems.py
@@ -49,3 +49,29 @@ print str_len("Lyndon")
 print str_len("Lawrence")
 print str_len2("Lyndon")
 print str_len2("Lawrence")
+
+# check last letter
+
+def last_letter(my_string):
+    return my_string[-1]
+
+print last_letter("Lyndon")
+print last_letter("Pikachu")
+
+# find max of 2 numbers
+
+def max_number(num1, num2):
+    if num1 > num2:
+        return num1
+    else:
+        return num2
+
+print(max_number(2, 5))
+print(max_number(15, 4))
+
+# find max of 3 numbers
+
+def max_of_3(num1, num2, num3):
+    return max_number(max_number(num1, num2), num3)
+
+print(max_of_3(3, 5, 7))
