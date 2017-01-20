226ea127b8712af06f4c84edc3c05dbdc4e56d92
lyndonl3091 <lyndonl3091@gmail.com>
2017-01-19 19:11:01 -0500

    strings

--- basics/index.py
+++ basics/index.py
@@ -25,3 +25,27 @@ def check_even(num):
 
 print check_even(2)
 print check_even(5)
+
+# check if odd
+
+def check_odd(num):
+    return num % 2 != 0
+
+print check_odd(3)
+print check_odd(6)
+
+# string length
+
+def str_len(my_string):
+    return len(my_string)
+
+def str_len2(my_string):
+    count = 0
+    for letter in my_string:
+        count += 1
+    return count
+
+print str_len("Lyndon")
+print str_len("Lawrence")
+print str_len2("Lyndon")
+print str_len2("Lawrence")
